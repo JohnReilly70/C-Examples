61a98efdc3d34cc0797e8450342d29c6f9642d8c
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-04-14 09:28:32 +0100

    Finished simple functions

--- Functions/ConsoleApplication1/ConsoleApplication1/LearningFunctions.cpp
+++ Functions/ConsoleApplication1/ConsoleApplication1/LearningFunctions.cpp
@@ -2,10 +2,34 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <time.h>
 
+using namespace std;
+
+int addNums(int first, int second, int third = 0) {
+	return first + second + third;
+}
+
+
+int getFactorial(int number) {
+	int sum;
+
+	if (number == 1) sum = 1;
+	else sum = getFactorial(number - 1)*number;
+	return sum;
+}
 
 int main()
 {
+	cout << addNums(1,2) << endl;
+	cout << addNums(1, 2, 3) << endl;
+
+	cout << getFactorial(2) << endl;
+
     return 0;
 }
 
