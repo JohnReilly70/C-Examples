c8877bb550ca03f7461a600b28f61e8c9ba7366a
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-04-14 09:44:24 +0100

    Initial commit

---
+++ Pointers/Pointers/Pointers.cpp
@@ -0,0 +1,18 @@
+// Pointers.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
+
